585d9ba1a293f70a37c4c0baeada2cb41d0a1f1a
Alex Collino <arcollino@gmail.com>
2022-10-25 16:26:58 -0700

    remove commented lines

--- makeText.js
+++ makeText.js
@@ -19,16 +19,13 @@ function markovFromFile(path) {
 
 async function markovFromURL(url) {
   if (!url) {
-    // console.log("INVALID INPUT: A valid url is required.");
     return "INVALID INPUT: A url is required.";
   } else {
     try {
       let resp = await axios.get(url);
       let markov = new MarkovMachine(resp.data);
-      // console.log(markov.makeText());
       return markov.makeText();
     } catch (err) {
-      // console.log(`Error fetching ${url}:\n`, err.message);
       return `Error requesting from ${url}\n${err.message}`;
     }
   }
@@ -37,7 +34,7 @@ if (require.main === module) {
   if (process.argv[2] === "file") {
     console.log(markovFromFile(process.argv[3]));
   } else if (process.argv[2] === "url") {
-    console.log(markovFromURL(process.argv[3]));
+    markovFromURL(process.argv[3]).then((text) => console.log(text));
   } else {
     console.log(
       "INVALID ARGUMENT: Must specify whether the input is a file or URL."
