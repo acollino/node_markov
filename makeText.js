7982e02c5e9b6d9bff35d84f16945559ed6f4d26
Alex Collino <arcollino@gmail.com>
2022-10-25 16:15:25 -0700

    change where text is logged to enabled testing

--- makeText.js
+++ makeText.js
@@ -1,43 +1,48 @@
 /** Command-line tool to generate Markov text. */
 const { MarkovMachine } = require("./markov");
 const axios = require("axios");
-const { readFile } = require("fs");
+const { readFileSync } = require("fs");
 
 function markovFromFile(path) {
   if (!path) {
-    console.log("INVALID INPUT: Path to file is required.");
+    return "INVALID INPUT: Path to file is required.";
   } else {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        console.log(`Error reading from file`, err.message);
-      } else {
-        let markov = new MarkovMachine(data);
-        console.log(markov.makeText());
-      }
-    });
+    try {
+      let data = readFileSync(path, "utf8");
+      let markov = new MarkovMachine(data);
+      return markov.makeText();
+    } catch (err) {
+      return `Error reading from ${path}\n${err.message}`;
+    }
   }
 }
 
 async function markovFromURL(url) {
   if (!url) {
-    console.log("INVALID INPUT: A valid url is required.");
+    // console.log("INVALID INPUT: A valid url is required.");
+    return "INVALID INPUT: A url is required.";
   } else {
     try {
       let resp = await axios.get(url);
       let markov = new MarkovMachine(resp.data);
-      console.log(markov.makeText());
+      // console.log(markov.makeText());
+      return markov.makeText();
     } catch (err) {
-      console.log(`Error fetching ${url}:\n`, err.message);
+      // console.log(`Error fetching ${url}:\n`, err.message);
+      return `Error requesting from ${url}\n${err.message}`;
     }
   }
 }
-
-if (process.argv[2] === "file") {
-  markovFromFile(process.argv[3]);
-} else if (process.argv[2] === "url") {
-  markovFromURL(process.argv[3]);
-} else {
-  console.log(
-    "INVALID ARGUMENT: Must specify whether the input is a file or URL."
-  );
+if (require.main === module) {
+  if (process.argv[2] === "file") {
+    console.log(markovFromFile(process.argv[3]));
+  } else if (process.argv[2] === "url") {
+    console.log(markovFromURL(process.argv[3]));
+  } else {
+    console.log(
+      "INVALID ARGUMENT: Must specify whether the input is a file or URL."
+    );
+  }
 }
+
+module.exports = { markovFromFile, markovFromURL };
